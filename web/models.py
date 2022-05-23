32a10ca7199896ea80a804dc21b839d4a4b5aa3d
Tarun Luthra <tarun.luthra@scaler.com>
2022-05-23 17:51:16 +0530

    Like model

--- web/models.py
+++ web/models.py
@@ -78,3 +78,13 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.follower} follows {self.followee}"
+
+
+class Like(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} likes {self.article}"
